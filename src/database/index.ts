57a14a65290ae3f8a637b586e19cd7ed5104af8e
Sergienko Victor <vik.sergienko@mail.ru>
2023-11-25 10:34:36 +0100

    init

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,22 +6,21 @@ dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL as string);
 
-  const umzug = new Umzug({
+const umzug = new Umzug({
     migrations: [],
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: console,
-  });
+});
 
   (async () => {
     try {
-    sequelize.authenticate();
-    sequelize.sync()
-    await umzug.up();
+      sequelize.authenticate();
+      sequelize.sync()
+      await umzug.up();
 
-    }catch(e) {
+    } catch(e) {
         console.error(e)
         process.exit(1);
     }
   })();
-
